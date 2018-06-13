b5c71ba0452d64788dd3bf3b396eee8e55209283
Alexey Serebryakov <silwar40@gmail.com>
2018-06-13 17:35:18 +0300

    js don't work

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,9 +4,11 @@ import whatInput from 'what-input';
 window.$ = $;
 
 import Foundation from 'foundation-sites';
-// If you want to pick and choose which modules to include, comment out the above and uncomment
-// the line below
-//import './lib/foundation-explicit-pieces';
 
+$(window).on('load', function (){
+	$(document).foundation();
 
-$(document).foundation();
+	$('#toTheTop').click(function() {
+		$.smoothScroll({ scrollTarget: $('#header') });
+	});
+});
