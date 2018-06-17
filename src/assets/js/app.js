e0d6ced349f4b76670090e7890a458a13a3a0d4c
Alexey Serebryakov <silwar40@gmail.com>
2018-06-17 19:38:07 +0300

    only orbit customize not ready

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,4 +7,20 @@ import Foundation from 'foundation-sites';
 
 $(window).on('load', function (){
 	$(document).foundation();
+
+	let menu = [['#homeButton', '#homeMenu'], ['#megaButton', '#megaMenu']];
+
+	for (let i = 0; i < menu.length; i++) {
+		let hoveredButton = menu[i][0];
+		let openedMenu = menu[i][1];
+
+		$([hoveredButton, openedMenu].join()).hover(function() {
+			$(openedMenu).css('display', 'block');
+			$(hoveredButton).addClass('hovered');
+		},
+			function () {
+			$(openedMenu).css('display', 'none');
+			$(hoveredButton).removeClass('hovered');
+		});
+	}
 });
