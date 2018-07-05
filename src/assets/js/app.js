23fc3ca06a422cdf01aeb827ea049fea5177d1b1
Aleksey Serebruakov <silwar40k@gmail.com>
2018-07-06 00:17:35 +0300

    in works 3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,6 +5,31 @@ window.$ = $;
 
 import Foundation from 'foundation-sites';
 
+
 $(window).on('load', function (){
     $(document).foundation();
+    $(".owl-carousel").owlCarousel(
+        {
+            loop:true,
+            margin:30,
+            nav:true,
+            autoWidth:true,
+            //responsiveClass:true,
+            responsive:{
+                0:{
+                    items:2,
+                    nav:true
+                },
+                640:{
+                    items:3,
+                    nav:true
+                },
+                1023:{
+                    items:4,
+                    nav:true,
+                    loop:true
+                }
+            }
+        }
+    );
 });
